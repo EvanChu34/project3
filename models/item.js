a6eacf909d9815640396a957342f2b6d9ceb8ec0
Evan Chu <echu34@gmail.com>
2021-04-20 19:44:30 -0400

    modified to fit application

--- models/item.js
+++ models/item.js
@@ -2,9 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const itemSchema = new Schema({
-  title: { type: String, required: true },
+  item: { type: String, required: true },
   author: { type: String, required: true },
   description: String,
+  location: { type: String, required: true},
   date: { type: Date, default: Date.now }
 });
 
