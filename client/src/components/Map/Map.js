c8679ef04916bedbeb1fed0dabb9529d39ef16a2
fiachmchh <fiachmchh@gmail.com>
2021-04-26 14:38:11 -0400

    mongo and maps working

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -1,22 +1,44 @@
 import React, { useEffect, useState } from 'react';
 import { GoogleMap, LoadScript, Marker} from '@react-google-maps/api';
 import mapStyles from './mapStyles.js';
+import { usePosition } from 'use-position';
+import Axios from 'axios';
 
 export const MapContainer = () => {
+  // require('dotenv').config()
 
-  const [ currentPosition, setCurrentPosition ] = useState({});
+  const [ currentPosition, setCurrentPosition ] = useState({
+    lat: 40.75, lng: -73.97
+  });
+
+  const [items, setItems] = useState([])
   
   const success = position => {
+    console.log('Scucress aheppening ??', position)
     const currentPosition = {
       lat: position.coords.latitude,
       lng: position.coords.longitude
     }
     setCurrentPosition(currentPosition);
   };
+  const err = error => {
+    console.log('Err!!!'. error)
+  }
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(success);
-  })
+
+    Axios.get('/allItems').then(function(items) {
+      console.log('AL ITEMS FROM DB', items)
+      setItems(items.data)
+
+    })
+    // navigator.geolocation.getCurrentPosition(success, err)
+    // var id = navigator.geolocation.watchPosition(success)
+    // console.log('id geocord e', id)
+    
+  }, [])
+  
+
 
   const options = {
     styles: mapStyles,
@@ -25,8 +47,11 @@ export const MapContainer = () => {
   };
 
   const onMarkerDragEnd = (e) => {
+
     const lat = e.latLng.lat();
     const lng = e.latLng.lng();
+
+    console.log('DRAG LAT AND LONG', lat, lng)
     setCurrentPosition({ lat, lng})
   };
   
@@ -37,24 +62,54 @@ export const MapContainer = () => {
   const defaultCenter = {
     lat: 40.75, lng: -73.97
   }
+
+  // const { REACT_APP__GOOGLE_MAPS_API_KEY } = process.env;
   
+  console.log('items ARRAY form db', items)
   return (
-     <LoadScript googleMapsApiKey='AIzaSyCyxlzFEbo5T--hz572BoW0NYxkujEzn_s'>
+    <div>
+      
+     <LoadScript googleMapsApiKey = {'AIzaSyCyxlzFEbo5T--hz572BoW0NYxkujEzn_s'}>
         <GoogleMap
           mapContainerStyle={mapSize}
           zoom={12}
           options={options}
-          center={currentPosition}>
-            {
-            currentPosition.lat ? 
-            <Marker
-            position={currentPosition}
+          center={defaultCenter}
+          // onClick={(event) => {
+            
+          //   setMarkers(current => [
+          //     ...current, 
+          //     {
+          //     lat: event.latlng.lat(),
+          //     lng: event.latlng.lng(),
+          //     time: new Date()
+            
+          //   },
+          // ]);
+          // }}
+          
+          >
+            {items.map((singleItem)=>{
+              console.log('ITMES LOOP HAPPENING!!')
+              return (
+                <Marker
+            position={{lat: singleItem.location.lat, lng: singleItem.location.lng}}
             onDragEnd={(e) => onMarkerDragEnd(e)}
-            draggable={true} /> :
-            null
+            draggable={true} />
+              )
+            })}
+            
+            {
+            // currentPosition.lat ? 
+            // <Marker
+            // position={currentPosition}
+            // onDragEnd={(e) => onMarkerDragEnd(e)}
+            // draggable={true} /> :
+            // null
           }
      </GoogleMap>
      </LoadScript>
+     </div>
   )
 }
 
